053507bcf67435531b505cf1c291d840ccb8b3ad
TerezaLic <34028623+TerezaLic@users.noreply.github.com>
2020-01-29 21:47:58 +0100

    Update magic.js

--- magic.js
+++ magic.js
@@ -6,7 +6,7 @@
 /*var orchestrationId = urlParam("id");*/
 var orchestrationId ="59937776"
 /*var token = urlParam("token"); */
-var token = 
+var token ="457-40757-6IerOHSJFdNZPH35i89Hrtl0BxkbIYxC8X1em0bF"
 var waiting = 0;
 var loop;
 
